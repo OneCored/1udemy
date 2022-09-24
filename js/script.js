22f8e6f0eee899f5ad19778d5cd2ed03be31b3b8
Sandris <pr_sandris@mail.com>
2022-09-24 18:52:03 +0300

    Условия. Урок 19 и 20

--- js/script.js
+++ js/script.js
@@ -9,13 +9,3 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
