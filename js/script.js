2f57ea9db8fcf585fe2487e5e35645525e0a2cb8
Sandris <pr_sandris@mail.com>
2022-09-24 18:56:06 +0300

    правка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,59 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'Введите');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'Введите');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+
+// if () {
+//     console.log('Ok!');
+// } else {
+//     console.log('Ne ok!!!');
+// }
+
+
+// if (num < 49) {
+//     console.log('net');
+// } else if (num < 100) {
+//     console.log('mnogo');
+// } else {
+//     console.log('da');
+// }
+
+// (num === 50) ? console.log('DADA') : console.log('NETNET');
+
+// const num = '50';
+
+// switch (num) {
+//     case 49:
+//         console.log('Neverno');
+//         break;
+//     case '100':
+//         console.log('Neverno');
+//         break;
+//     case '50':
+//         console.log('VERNO');
+//         break;
+//     default:
+//         console.log('SORYAN');
+//         break;
+// };
 
 const personalMovieDB = {
     count: numberOfFilms,
